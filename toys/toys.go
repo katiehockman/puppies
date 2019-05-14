d3355e67efa8bc9f82b9ebff5a6172aecfd35b3b
Katie Hockman <katie@golang.org>
2019-05-14 14:24:12 +0900

    add dependencies for side effects and run go mod tidy to have a valid go.mod and go.sum file

--- toys/toys.go
+++ toys/toys.go
@@ -1,7 +1,11 @@
 package toys
 
-import "fmt"
-
+import (
+       "fmt"
+	_ "golang.org/x/crypto/acme"
+	_ "github.com/go-sql-driver/mysql"
+	_ "go.opencensus.io/tag"
+)
 // Reminder reminds you to buy your dog a toy.
 func Reminder() {
 	fmt.Print("go buy a toy for your dog!!")
--- walk/walk.go
+++ walk/walk.go
@@ -1,5 +1,11 @@
 package walk
 
+import (
+	_ "golang.org/x/crypto/acme"
+	_ "github.com/go-sql-driver/mysql"
+	_ "go.opencensus.io/tag"
+)
+
 // BestRoute tells you the best dog walking route in your neighborhood.
 func BestRoute() string {
 	return "the best place to walk your dog is by your side"
